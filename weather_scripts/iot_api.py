704dca32014d99d9eba0da29dce86ca57b2d106d
Hajnal Balint <hajnal.balint.david@gmail.com>
2022-11-01 23:50:59 +0100

    add iot weather codes

---
+++ weather_scripts/iot_api.py
@@ -0,0 +1,36 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+import sys 
+import time
+
+app = FastAPI()
+
+currentTemperature : int = 0
+
+def getTemperature():
+    temperature = 10
+    #humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+    time.sleep(5)
+    if temperature is not None:
+        print(temperature)
+        return temperature
+    else:
+        print('Failed to get reading. Try again!')
+        #sys.exit(1)
+
+
+@app.get("/")
+def read_root():
+    return {"Welcome": "To the IoT weather node"}
+
+@app.get("/weather")
+def get_weather():
+    return {"temperature": getTemperature()}
+
+# bad?
+@app.get("/async_weather")
+async def get_weather_async():
+    result = await getTemperature()
+    return {"temperature": result}
