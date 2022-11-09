733deacfd23f742c1428c0c4f2e0cf533df0e441
Balint_ubuntu <hajnal.balint.david@gmail.com>
2022-11-09 13:01:31 +0100

    enable sensor in api

--- weather_scripts/iot_api.py
+++ weather_scripts/iot_api.py
@@ -3,25 +3,31 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import sys 
 import time
+import Adafruit_DHT
+
 
 app = FastAPI()
 
-# how to start server: 
+# how to start server for dev: 
 # uvicorn iot_api:app --reload
+
+# how to start server for prod: 
+# uvicorn iot_api:app --host 0.0.0.0 -port 1234
+# access: server_ip:1234
 
 currentTemperature : int = 0
 
 def getTemperature():
-    temperature = 10
-    #humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    temperature = None
+    humidity, temperature = Adafruit_DHT.read_retry(11, 2)
 
-    time.sleep(5)
+    # time.sleep(5)
     if temperature is not None:
         print(temperature)
         return temperature
     else:
         print('Failed to get reading. Try again!')
-        #sys.exit(1)
+        return "Failed to get reading"
 
 
 @app.get("/")
